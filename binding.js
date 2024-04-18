d22629ea362bf4112121430458c6a2891b99c6b1
chebet-55 <164503773+chebet-55@users.noreply.github.com>
2024-04-18 14:00:51 +0300

    first commit

---
+++ binding.js
@@ -0,0 +1,10 @@
+//let f_ name="brina"
+//let s_name="bett"
+//let full_name=f_name+s_name
+//document.getElementById("first").innerHTML=full_name
+
+// getting html to js
+
+let form=document.getElementById("myform")
+
+form.addEventListener("submit")
